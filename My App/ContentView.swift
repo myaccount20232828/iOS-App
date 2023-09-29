152cd113034d0fdae93a874a69e66dc9f6b425e0
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2023-09-29 14:59:44 -0400

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -2,6 +2,15 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("test")
+    Text(GetImageFiles().description)
+  }
+}
+
+func GetImageFiles() -> [String] {
+  do {
+    return try FileManager.default.contentsOfDirectory(atPath: "/var/mobile/KEEP").filter({$0.hasSuffix(".jpg")})
+  } catch {
+    print(error)
+    return []
   }
 }
