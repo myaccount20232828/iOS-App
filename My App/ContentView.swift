c2c06354c73977eede6fbe346acfec983d5cbeb0
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2023-09-29 15:05:38 -0400

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -2,13 +2,25 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text(GetImageFiles().description)
+    ForEach(GetImages(), id: \.self) { image in {
+      Image(uiimage: image)
+    }
   }
+}
+
+let ImagesPath = "/var/mobile/KEEP"
+
+func GetImages() -> [UIImage] {
+  var Images: [UIImage] = []
+  for ImageName in GetImageFiles() {
+    Images.append(UIImage(contentsOfFile: "\(ImagesPath)/\(ImageName)"))
+  }
+  return Images
 }
 
 func GetImageFiles() -> [String] {
   do {
-    return try FileManager.default.contentsOfDirectory(atPath: "/var/mobile/KEEP").filter({$0.hasSuffix(".jpg")})
+    return try FileManager.default.contentsOfDirectory(atPath: ImagesPath).filter({$0.hasSuffix(".jpg")})
   } catch {
     print(error)
     return []
