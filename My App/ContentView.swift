77e5d6025be9d01341d5f6661f27228467ec708b
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2023-09-29 15:10:11 -0400

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    ForEach(GetImages(), id: \.self) { CurrentImage in {
+    ForEach(GetImages(), id: \.self) { CurrentImage in
       Image(uiImage: CurrentImage)
     }
   }
